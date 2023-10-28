517949c4c38d1a7749e70c73782e0b9ac7d36a17
Levi Bohn <levibohn@gmail.com>
2023-10-28 12:44:57 -0500

    adds in addOne.

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,14 @@ import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 import reducer, { initialState } from './../reducers/index';
+import { addOne } from './../actions';
 
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  const handle1Click = ()=>{
+    dispatch(addOne());
+  }
   
   return (
     <div className="App">
@@ -23,7 +28,7 @@ function App() {
             <TotalDisplay value={state.total}/>
             <div className="row details">
               <span id="operation"><b>Operation:</b> {state.operation} </span>
-              <span id="memory"><b>Memory:</b> (state.memory) </span>
+              <span id="memory"><b>Memory:</b> {state.memory} </span>
             </div>
             
             <div className="row">
@@ -33,7 +38,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handle1Click}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
